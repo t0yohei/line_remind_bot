6c504b694e64c76ef8799fbb92d7bcd611628ea7
toyo-hei <inuneko690121@gmail.com>
2018-05-24 22:43:46 +0900

    一日だけを選択した時の返答を追加

--- Gemfile
+++ Gemfile
@@ -20,6 +20,9 @@ group :development, :test do
   gem 'binding_of_caller'
   gem 'rspec-rails'
   gem 'rails-controller-testing'
+  gem 'factory_girl_rails'
+  gem 'guard-rspec'
+  gem 'spring-commands-rspec'
 end
 
 group :development do
@@ -27,6 +30,15 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'faker'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -14,10 +14,18 @@ class WebhookController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
+          input_text = event.message['text']
+          if input_text == '一日だけ'
+            message = {
+              type: 'text',
+              text: '日付を入力してください'
+            }
+          else
+            message = {
+              type: 'text',
+              text: event.message['text'] + '正しく入力してください'
+            }
+          end
           client.reply_message(event['replyToken'], message)
         end
       end
