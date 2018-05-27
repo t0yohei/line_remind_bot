961c3aaf8b6b3bdc3d1141c254df31425a166a56
toyo-hei <inuneko690121@gmail.com>
2018-05-27 16:41:44 +0900

    画面実装、一応形だけOK

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -12,22 +12,18 @@ class WebhookController < ApplicationController
     events.each { |event|
       case event
       when Line::Bot::Event::Postback
-        message = {
-          type: 'text',
-          text: '入力されました'
-        }
-        # case event['postback']['params']
-        # when Line::Bot::Event::Postback::Params::Datetime
-        #   message = {
-        #     type: 'text',
-        #     text: event['postback']['params']['datetime'] + 'が入力されました'
-        #   }
-        # when Line::Bot::Event::Postback::Params::Time
-        #   message = {
-        #     type: 'text',
-        #     text: event['postback']['params']['time'] + 'が入力されました'
-        #   }
-        # end
+        case event['postback']['params'].first.first
+        when /datetime/
+          message = {
+            type: 'text',
+            text: event['postback']['params']['datetime'] + 'が入力されました'
+          }
+        when /time/
+          message = {
+            type: 'text',
+            text: event['postback']['params']['time'] + 'が入力されました'
+          }
+        end
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
@@ -36,49 +32,51 @@ class WebhookController < ApplicationController
           when '予定を登録'
             message = {
               type: 'text',
-              text: "タイトルとカテゴリを入力してください\n例：\n海水浴\n毎日"
+              text: 'タイトルとカテゴリを入力してください' \
+              "\nカテゴリ：「一日だけ」・「毎日」・「毎週」・「毎月」\n例：\n海水浴\n毎日"
             }
-          when '一日だけ'
+          when /一日だけ/
             message = {
-              type: 'text',
-              text: 'タイトルを入力してください'
+              type: 'template',
+              altText: 'this is an template message',
+              template: {
+                type: 'buttons',
+                title: '一日だけ',
+                text: event.message['text'],
+                actions: [
+                  {
+                    type: 'datetimepicker',
+                    label: '日時を指定',
+                    data: event.message['text'],
+                    mode: 'datetime'
+                  }
+                ]
+              }
             }
-            # message = {
-            #   type: 'template',
-            #   altText: 'this is an template message',
-            #   template: {
-            #     type: 'buttons',
-            #     title: '一日だけ',
-            #     text: event.message['text'] + '日時を指定',
-            #     actions: [
-            #       {
-            #         type: 'datetimepicker',
-            #         label: '日時を指定',
-            #         data: event.message['text'] + '一日だけ',
-            #         mode: 'datetime',
-            #       }
-            #     ]
-            #   }
-            # }
-          when '毎日'
+          when /毎日/
             message = {
               type: 'template',
               altText: 'this is an template message',
               template: {
                 type: 'buttons',
                 title: '毎日',
-                text: '時間を指定',
+                text: event.message['text'],
                 actions: [
                   {
                     type: 'datetimepicker',
                     label: '時間を指定',
-                    data: '毎日',
-                    mode: 'time',
+                    data: event.message['text'],
+                    mode: 'time'
                   }
                 ]
               }
             }
-          when '毎週'
+          when /毎週/ && /曜日/
+            message = {
+              type: 'text',
+              text: event.message['text'] + '入力されました'
+            }
+          when /毎週/ && /その他/
             message = {
               type: 'template',
               altText: 'this is an template message',
@@ -86,78 +84,78 @@ class WebhookController < ApplicationController
                 type: 'buttons',
                 title: '曜日選択',
                 # thumbnailImageUrl: 'https://example.com/image.jpg',
-                text: '曜日を選択してください',
-                actions: [
-                  {
-                    type: 'message',
-                    label: '月曜日',
-                    text: '月曜日'
-                  },
-                  {
-                    type: 'message',
-                    label: '火曜日',
-                    text: '火曜日'
-                  },
-                  {
-                    type: 'message',
-                    label: '水曜日',
-                    text: '水曜日'
-                  },
-                  {
-                    type: 'message',
-                    label: 'その他の曜日',
-                    text: 'その他の曜日'
-                  }
-                ]
-              }
-            }
-          when 'その他の曜日'
-            message = {
-              type: 'template',
-              altText: 'this is an template message',
-              template: {
-                type: 'buttons',
-                title: '曜日選択',
-                # thumbnailImageUrl: 'https://example.com/image.jpg',
-                text: '曜日を選択してください',
+                text: event.message['text'] + "\n曜日を選択してください",
                 actions: [
                   {
                     type: 'message',
                     label: '木曜日',
-                    text: '木曜日'
+                    text: event.message['text'] + '木曜日'
                   },
                   {
                     type: 'message',
                     label: '金曜日',
-                    text: '金曜日'
+                    text: event.message['text'] + '金曜日'
                   },
                   {
                     type: 'message',
                     label: '土曜日',
-                    text: '土曜日'
+                    text: event.message['text'] + '土曜日'
                   },
                   {
                     type: 'message',
                     label: '日曜日',
-                    text: '日曜日'
+                    text: event.message['text'] + '日曜日'
                   }
                 ]
               }
             }
-          when '毎月'
+          when /毎週/
+            message = {
+              type: 'template',
+              altText: 'this is an template message',
+              template: {
+                type: 'buttons',
+                title: '曜日選択',
+                # thumbnailImageUrl: 'https://example.com/image.jpg',
+                text: event.message['text'],
+                actions: [
+                  {
+                    type: 'message',
+                    label: '月曜日',
+                    text: event.message['text'] + '月曜日'
+                  },
+                  {
+                    type: 'message',
+                    label: '火曜日',
+                    text: event.message['text'] + '火曜日'
+                  },
+                  {
+                    type: 'message',
+                    label: '水曜日',
+                    text: event.message['text'] + '水曜日'
+                  },
+                  {
+                    type: 'message',
+                    label: 'その他の曜日',
+                    text: event.message['text'] + 'その他'
+                  }
+                ]
+              }
+            }
+          when /毎月/
             message = {
               type: 'template',
               altText: 'this is an template message',
               template: {
                 type: 'buttons',
                 title: '毎月',
-                text: '日時を指定',
+                text: event.message['text'],
                 actions: [
                   {
                     type: 'datetimepicker',
                     label: '日時を指定',
-                    data: '毎月',
-                    mode: 'datetime',
+                    data: event.message['text'],
+                    mode: 'datetime'
                   }
                 ]
               }
