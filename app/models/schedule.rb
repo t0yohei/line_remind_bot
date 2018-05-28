d3069f7c469dab9612c88bbf54a1b7cb7f7cf0c4
toyo-hei <inuneko690121@gmail.com>
2018-05-28 23:46:45 +0900

    enumを設定

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,10 @@
 class Schedule < ApplicationRecord
   belongs_to :talk_room_type
+  enum schedule_type:
+    { specific_day: 'specific_day', daily: 'daily', weekly: 'weekly',
+      monthly: 'monthly' }
+  enum post_day:
+    { Monday: 'Monday', Tuesday: 'Tuesday', Wednesday: 'Wednesday',
+      Thursday: 'Thursday', Friday: 'Friday', Saturday: 'Saturday',
+      Sunday: 'Sunday' }
 end
